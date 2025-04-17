86c3d17c8edf5815392eb6f437d4b3113d43511f
Joaquin Pastor <joaquinpastorsuiffet@gmail.com>
2025-04-17 18:16:37 -0300

    feat: sign in practice

--- app/src/main/java/com/example/teamup/MainActivity.java
+++ app/src/main/java/com/example/teamup/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.teamup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button newBtn = findViewById(R.id.newbtn);
+        newBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView newMsg = findViewById(R.id.signInText);
+                newMsg.setText("Crear cuenta");
+            }
+        });
     }
+
+    public void updateMsg(View view){
+        Log.i("testMessage", "Update Msg");
+        //R.id.signInText
+        TextView signInText = findViewById(R.id.signInText);
+        signInText.setText(R.string.newMesg);
+    }
+
+
 }
